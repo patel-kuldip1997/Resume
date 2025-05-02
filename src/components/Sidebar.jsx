1795181da850f74752b562a0e2c14a0e2b185308
patel-kuldip1997 <patelkuldip1997@gmail.com>
2025-05-02 11:10:12 +0530

    CV added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,7 +19,7 @@ const Sidebar = () => {
           <a className="btn btn-secondary btn-square mx-1" href="#"><i className="fab fa-instagram"></i></a>
         </div>
         <div className="d-flex align-items-end justify-content-between border-top">
-        <a href="/assets/Report.pdf" download className="btn w-50 border-end">Download CV</a>
+        <a href="/assets/Kuldipkumar_Patel_5YOE.pdf" download className="btn w-50 border-end">Download CV</a>
         <Link to="contact" className="btn w-50 btn-scroll"   onClick={() => {
         document.getElementById("contact")?.scrollIntoView({ behavior: "smooth" });}}>Contact Me</Link>
         </div>
